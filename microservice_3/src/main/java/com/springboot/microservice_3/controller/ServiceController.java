b19541a39c5b7db54f013d4f02c47f5d46a4c1b3
Kang Dong Hyeon <ehdgus6435@kumoh.ac.kr>
2025-05-26 15:02:47 +0900

    init : 프로젝트 생성 및 간단한 api 생성

---
+++ microservice_3/src/main/java/com/springboot/microservice_3/controller/ServiceController.java
@@ -0,0 +1,15 @@
+package com.springboot.microservice_3.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class ServiceController {
+
+    @GetMapping("/service-1")
+    public String Hello() {
+        return "third-service-return";
+    }
+}
